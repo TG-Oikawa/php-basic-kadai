9c1b9d6963fc6ae176e8c4b7439a1f28c51c2f6c
TG-Oikawa <98362436+TG-Oikawa@users.noreply.github.com>
2023-12-16 14:58:53 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編 課題 連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+    <p>
+        <?php
+            $Array = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+            print_r($Array);
+            ?>
+    </p>
+</body>
+</html>
